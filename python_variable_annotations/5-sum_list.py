b2fbecec49445a2b4ea6763c809ed8cdf9742c65
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-08 09:47:42 +0200

    Fix : change place of the module docstring

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import List
-
 """ type-annotated function which takes a list of floats as
 argument and returns their sum as a float"""
+
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
