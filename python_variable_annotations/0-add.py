f238e1e1830b0d4e620d70e236ea8d0dd3f52576
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-08 09:16:00 +0200

    Fix : add docstring for the module

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+""" add that takes a float a and a float b as arguments
+    and returns their sum as a float"""
+
 
 def add(a: float, b: float) -> float:
-    """ add that takes a float a and a float b as arguments
-    and returns their sum as a float"""
+    """Return the sum of a and b."""
     return a + b
