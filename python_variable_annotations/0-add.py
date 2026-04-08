86fc3bed5388616c1e75caa95838ab79ba152c3c
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-08 09:13:07 +0200

    Fix :  change place of the docstring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-""" add that takes a float a and a float b as arguments
-and returns their sum as a float"""
-
 
 def add(a: float, b: float) -> float:
+    """ add that takes a float a and a float b as arguments
+    and returns their sum as a float"""
     return a + b
