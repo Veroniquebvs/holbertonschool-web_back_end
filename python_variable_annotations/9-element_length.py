5dc5beb8d924e4aa41e2cb8bb363af30015ea6a2
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-08 09:52:33 +0200

    Fix : change place of the module docstring

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+""" Return a list of tuples containing each element and its length."""
+
 from typing import Iterable
 from typing import Sequence
 from typing import List
 from typing import Tuple
-
-""" Return a list of tuples containing each element and its length."""
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
