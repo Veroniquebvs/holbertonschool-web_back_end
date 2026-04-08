a5ddd68532d6334301c5d598ed0d737230f8443a
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-08 09:49:41 +0200

    Fix : change place of the module docstring

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from typing import Union
-from typing import Tuple
-
 """  type-annotated function that takes a string and
 an int OR float as arguments and returns a tuple"""
+
+from typing import Union
+from typing import Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
