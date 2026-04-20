901f9644d2a3e45faec82911c2f1ae0886256d89
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-20 15:10:41 +0200

    style : add uppercase to Tuple

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@
 
 from typing import Tuple
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Return a tuple of two integers representing the start and end index of the page """
 
     start = (page - 1) * page_size
