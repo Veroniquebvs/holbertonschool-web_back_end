ec4500480528c07544dfc380026e94fa891b6818
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-20 15:32:51 +0200

    Fix : changed the formula of the variable

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -7,5 +7,5 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Return a tuple of two integers representing the start and end index of the page """
 
     start = (page - 1) * page_size
-    end = start * page_size
+    end = page * page_size
     return (start, end)
