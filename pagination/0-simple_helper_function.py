d522f2ca1a4a6422d7c3b31b13077b8d1245eea4
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-20 15:38:08 +0200

    docs : changed docstring's presentation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-""" Simple helper function """
+"""
+Simple helper function
+"""
 
 from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple:
-    """ Return a tuple of two integers representing the start and end index of the page """
+    """
+    Return a tuple of two integers representing the start and end index of the page
+    """
 
     start = (page - 1) * page_size
     end = page * page_size
