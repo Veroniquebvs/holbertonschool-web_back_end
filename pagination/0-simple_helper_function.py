737151c826b2968ee46f33238195a5d108421eb1
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-20 15:44:29 +0200

    style : delete a space

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,7 +8,7 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple:
     """
-    Return a tuple of two integers representing 
+    Return a tuple of two integers representing
     the start and end index of the page
     """
 
