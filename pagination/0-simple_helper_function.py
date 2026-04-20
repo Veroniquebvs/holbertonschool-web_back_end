05a928a9b21e57ed7bd0bdc9e73c01ab95283fc3
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-20 15:35:32 +0200

    refactor : return a Tuple only

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@
 
 from typing import Tuple
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple:
     """ Return a tuple of two integers representing the start and end index of the page """
 
     start = (page - 1) * page_size
