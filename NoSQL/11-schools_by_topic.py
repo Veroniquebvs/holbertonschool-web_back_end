cdac3149b4bc72ee64349028a031bca2af7b1518
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-21 15:06:32 +0200

    feat : change the result in a list

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -5,4 +5,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """ return a list with a specific topic"""
-    return (mongo_collection.find({"topic": topic}))
+    return (list(mongo_collection.find({"topics": topic})))
