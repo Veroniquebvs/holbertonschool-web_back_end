c0d1935ac8c97faf8a59c4e204eb4ecbb412c4f6
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-21 18:01:22 +0200

    chore: initialize virtual environment dependencies for NoSQL project

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,18 +4,19 @@
 
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.logs
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client.logs
+    num_logs = db.nginx.count_documents({})
+    print(f"{num_logs} logs")
 
-num_logs = db.nginx.count_documents({})
-print(f"{num_logs} logs")
+    print("Methods:")
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for i in method:
+        count = db.nginx.count_documents({"method": i})
+        print(f"\tmethod {i}: {count}")
 
-print("Methods:")
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for i in method:
-    count = db.nginx.count_documents({"method": i})
-    print(f"\tmethod {i}: {count}")
-
-status_check = db.nginx.count_documents({"method": "GET", "path": "/status"})
-print(f"{status_check} status check")
+    status_check = db.nginx.count_documents(({"method": "GET",
+                                            "path": "/status"}))
+    print(f"{status_check} status check")
