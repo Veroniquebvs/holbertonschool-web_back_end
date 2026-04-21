7f63ce67b583cf058adc4519d34f23ab3af0c396
veroniquebvs <beauvais.veronique@gmail.com>
2026-04-21 15:01:17 +0200

    feat : create a function that return a list having a specific topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" function that returns the list of school having a specific topic """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ return a list with a specific topic"""
+    return (mongo_collection.find({"topic": topic}))
