56f9a58ececbae427ff9a132bfac46cc42421cd1
veroniquebvs <beauvais.veronique@gmail.com>
2026-05-06 21:36:52 +0200

    feat : create a function that returns a map of groceries

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+    const myList = new Map();
+    myList.set('Apples', 10);
+    myList.set('Tomatoes', 10);
+    myList.set('Pasta', 1);
+    myList.set('Rice', 1);
+    myList.set('Banana', 5);
+
+    return myList;
+}
