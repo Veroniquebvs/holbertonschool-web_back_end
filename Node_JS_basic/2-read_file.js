f2508297e18203806951f0c9f3c8b313e3e8276a
veroniquebvs <beauvais.veronique@gmail.com>
2026-05-11 17:21:45 +0200

    feat : create a function that reads a file synchronously with Node JS

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const newArray = data.split('\n');
+    const students = newArray.slice(1).filter((line) => line.length > 0);
+    const fields = {};
+    students.forEach((line) => {
+      const itemStudents = line.split(',');
+      const firstname = itemStudents[0];
+      const field = itemStudents[3];
+
+      if (!(field in fields)) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    });
+
+    console.log(`Number of students: ${students.length}`);
+
+    const lastList = Object.keys(fields);
+    lastList.forEach((field) => {
+      const list = fields[field];
+      console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+    })
+
+  }
+  catch (e) {
+    throw new Error('Cannot load the database');
+  }
+};
+module.exports = countStudents;
