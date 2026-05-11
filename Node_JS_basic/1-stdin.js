8cf5dee8f644bb270cf3213509826501b6ee107d
veroniquebvs <beauvais.veronique@gmail.com>
2026-05-11 14:06:59 +0200

    fix : use process.stout.write

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,11 +2,8 @@ process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const input = data.trim();
-  console.log(`Your name is: ${input}`);
-  if (input === 'data') {
-    console.log('This important software is now closing');
-    process.exit(0);
-  } else {
-    process.exit(1);
-  }
+  process.stdout.write(`Your name is: ${input}\n`);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
