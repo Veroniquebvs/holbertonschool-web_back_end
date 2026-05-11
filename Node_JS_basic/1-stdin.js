dc2752dfac15a3a5f02bd465d41d4700b18bffab
veroniquebvs <beauvais.veronique@gmail.com>
2026-05-11 13:38:34 +0200

    refactor : add process.exit(1)

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,6 +3,10 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const input = data.trim();
   console.log(`Your name is: ${input}`);
-  console.log('This important software is now closing');
-  process.exit(0);
+  if (input === 'data') {
+    console.log('This important software is now closing');
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
 });
