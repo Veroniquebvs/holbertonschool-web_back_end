7b5240a7b7c7909306d5fbb832d4b7a7b1d4e9a8
veroniquebvs <beauvais.veronique@gmail.com>
2026-05-12 09:11:54 +0200

    fix : add process.stdin.pause

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,6 +3,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const input = data.trim();
   process.stdout.write(`Your name is: ${input}\n`);
+  process.stdin.pause();
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
