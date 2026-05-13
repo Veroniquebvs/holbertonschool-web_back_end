d55579b4f6b1210bbebfbd8455a36afbb653f4bb
veroniquebvs <beauvais.veronique@gmail.com>
2026-05-13 14:11:31 +0200

    style : delete useless spaces

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,26 +2,26 @@ const countStudents = require('./3-read_file_async');
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    if (req.url === '/students') {
-        res.write('This is the list of our students\n')
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n')
 
-        let output = '';
-        const newLog = console.log;
-        console.log = (msg) => { output += msg + '\n'; };
+    let output = '';
+    const newLog = console.log;
+    console.log = (msg) => { output += msg + '\n'; };
 
-        countStudents(process.argv[2])
-            .then(() => {
-                console.log = newLog;
-                res.end(output.trim());
-            })
-            .catch((err) => {
-                console.log = newLog;
-                res.end(err.message);
-            });
-    } else {
-        res.end('Hello Holberton School!');
-    }
+    countStudents(process.argv[2])
+      .then(() => {
+        console.log = newLog;
+        res.end(output.trim());
+      })
+      .catch((err) => {
+        console.log = newLog;
+        res.end(err.message);
+      });
+  } else {
+    res.end('Hello Holberton School!');
+  }
 });
 app.listen(1245);
 
